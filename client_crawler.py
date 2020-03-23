a2b9289b0334f78a06283c08e237a44b40e6d658
f0urteen <markzeng14@gmail.com>
2020-03-23 13:29:37 +0800

    fix: rewrite code according to iPython notebook

--- client_crawler.py
+++ client_crawler.py
@@ -30,37 +30,49 @@ indoorPages = range(1,867)
 landScapeLightList = urlList[3]
 landScapeLightPages = range(1,636)
 
-for p in indoorPages:
-    address = indoorList + str(p) + '.htm'
-    req = urllib3.PoolManager()
-    res = req.request('GET', address)
-    print(str(p)+'..')
-    soup = BeautifulSoup(res.data, 'html.parser')
-    suppliers = soup.find_all('div', attrs={'onmouseover': "this.className='itemcon2'"})
+def get_list(url, page_range, list_name):
 
-    for s in suppliers:
-        link = s.find('a', class_ = 'agreen')
-        if link is not None and link.text != "":
-            company_name.append(link.text)
-            # Extract location
-            a = re.search(r'\u3000', s.find('div', class_ = 'box2com').text)
-            if a is None:
-                a = re.search(r'(?<=\[\ ).*(?=\ \])', s.find('div', class_ = 'box2com').text)
-                company_locale_1.append(a[0])
-                company_locale_2.append(' ')
-            else:
-                a = re.search(r'(?<=\[\ )(.*)\u3000(.*)(?=\ \])', s.find('div', class_ = 'box2com').text)
-                company_locale_1.append(a[1])
-                company_locale_2.append(a[2])
-            # Extract type
-            a = re.search(r'(?<=\]\ \[\ ).*(?=\ \])', s.find('div', class_ = 'jyms').text)
-            company_type.append(a[0])
-            # Extract website
-            a = re.search(r'(?<=[\/]{2}).*', link.get('href'))
-            company_website.append(a[0])
-            # Extract product info
-            a = s.find_all('div', class_ = 'description')
-            company_product.append(re.sub(r'[主营：...]', '', a[1].text))
+    for p in page_range:
+        address = url + str(p) + '.htm'
+        req = urllib3.PoolManager()
+        res = req.request('GET', address)
+        print(str(p)+'..')
+        soup = BeautifulSoup(res.data, 'html.parser')
+        suppliers = soup.find_all('div', attrs={'onmouseover': "this.className='itemcon2'"})
 
-indoorSupplierList = pd.DataFrame({'公司名称': company_name, '地区': company_locale, '类型': company_type, '网站': company_website, '主营': company_product })
-indoorSupplierList.to_csv('indoorSupplierList.csv', encoding="utf_8_sig")
+        for s in suppliers:
+            link = s.find('a', class_ = 'agreen')
+            if link is not None and link.text != "":
+                company_name.append(link.text)
+                # Extract location
+                a = re.search(r'\u3000', s.find('div', class_ = 'box2com').text)
+                if a is None:
+                    a = re.search(r'(?<=\[\ ).*(?=\ \])', s.find('div', class_ = 'box2com').text)
+                    company_locale_1.append(a[0])
+                    company_locale_2.append(' ')
+                else:
+                    a = re.search(r'(?<=\[\ )(.*)\u3000(.*)(?=\ \])', s.find('div', class_ = 'box2com').text)
+                    company_locale_1.append(a[1])
+                    company_locale_2.append(a[2])
+                # Extract type
+                a = re.search(r'(?<=\]\ \[\ ).*(?=\ \])', s.find('div', class_ = 'jyms').text)
+                company_type.append(a[0])
+                # Extract website
+                a = re.search(r'(?<=[\/]{2}).*', link.get('href'))
+                company_website.append(a[0])
+                # Extract product info
+                a = s.find_all('div', class_ = 'description')
+                company_product.append(re.sub(r'[主营：...]', '', a[1].text))
+
+    clientList = pd.DataFrame({'公司名称': company_name, '省份': company_locale_1, '地区': company_locale_2, '类型': company_type, '网站': company_website, '主营': company_product })
+    clientList.to_csv(list_name, encoding='utf_8_sig')
+    
+    print('Done.')
+    
+    return clientList
+
+indoor_df = get_list(indoorList, indoorPages, '室内照明客户.csv')
+ledDriver_df = get_list(ledDriverList, ledDriverPages, 'LED驱动供应商.csv')
+clientdf = pd.concat([indoor_df, ledDriver_df], ignore_index=True)
+uniqueCdf = clientdf.drop_duplicates(subset='公司名称')
+clientdf.to_csv('合并客户列表.csv', encoding='utf_8_sig')
